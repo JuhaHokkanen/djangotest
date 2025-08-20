787bff89f968af25c0185d37b1f99df7dae1cdcb
Juha Hokkanen <juha.hokkanen@student.careeria.fi>
2025-08-20 10:48:05 +0300

    first commit

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def landingview(request):
+    return render(request, 'landingpage.html')
+
+
+def productlistview(request):
+    return render(request, 'productlist.html')
+
+def supplierlistview(request):
+    return render(request, 'supplierlist.html')
+    
