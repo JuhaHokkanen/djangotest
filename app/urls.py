041d5d3e9ab10f5e02f85e81f48487c2156bc925
Juha Hokkanen <juha.hokkanen@student.careeria.fi>
2025-08-25 11:16:04 +0300

    listaukset ja lisäykset toimii

--- app/urls.py
+++ app/urls.py
@@ -14,18 +14,30 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-from .views import landingview, productlistview, supplierlistview
 
+from app.views import landingview
+from .views import deletesupplier, landingview, productlistview, supplierlistview, deletesupplier, products_filtered
+from .views import addsupplier, addproduct, deleteproduct, confirmdeleteproduct, confirmdeletesupplier
+from .views import edit_product_post, edit_product_get, searchsuppliers
 
 urlpatterns = [
     path('', landingview),
-    
+
     # Products url´s
     path('products/', productlistview),
+    path('add-product/', addproduct),
+    path('delete-product/<int:id>/', deleteproduct),
+    path('confirm-delete-product/<int:id>/', confirmdeleteproduct),
+    path('products-by-supplier/<int:id>/', products_filtered),
+    path('edit-product-get/<int:id>/', edit_product_get),
+    path('edit-product-post/<int:id>/', edit_product_post), 
+    
 
     # Supplier url´s
     path('suppliers/', supplierlistview),
-
+    path('add-supplier/', addsupplier),
+    path('delete-supplier/<int:id>/', deletesupplier),
+    path('confirm-delete-supplier/<int:id>/', confirmdeletesupplier),
+    path('search-suppliers/', searchsuppliers),
 ]
